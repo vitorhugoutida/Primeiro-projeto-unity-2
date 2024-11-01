78899dfdd30e871436400f9a707d7febd70ba202
vitorhugoutida <vitorhugoutida@gmail.com>
2024-11-01 20:56:16 -0300

    feat: Atividade01 Finalizado

--- Assets/Scripts/Atividade01.cs
+++ Assets/Scripts/Atividade01.cs
@@ -3,30 +3,20 @@ using UnityEngine;
 public class Atividade01 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] public int VidaJogador = 100;
+    [SerializeField] public int vidaJogador = 100;
     void Start()
     {
         // (Verificação de pontos de vida) Um personagem perde pontos de vida após uma batalha. Verique se ele ainda está vivo. Caso tenha mais de 0 pontos de vida, exiba "Personagem Vivo", caso contrário, "Game Over"
-        if (VidaJogador < 0)
-        {
-            print("Personagem Vivo");
-        }
-        else if (VidaJogador == 0)
+        if (vidaJogador == 0)
         {
             print("Game Over");
+        } else {
+            print("Personagem Vivo");
         }
-
         // Update is called once per frame
         void Update()
         {
-
+        
         }
     }
 }
-Oi por favor coloca o códigos no Git hub para eu ver os códigos porque não copiei tudo
-
-vc precisa olhar o código agora
-
-vc precisa fazer duas verificações
-1 - se a vida for maior que 0 vivo
-se não morto
--- Assets/Scripts/Geral.cs
+++ Assets/Scripts/Geral.cs
@@ -1,20 +1,55 @@
-using UnityEngine;
+//public class Geral : MonoBehaviour
+//{
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-public class Geral : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //    if(vidaHeroi < vidaVilao)
-        //    {
-        //        resultado 
-        //    }
-        //}
+//    //Operadores relacionais
 
-        // Update is called once per frame
-        void Update()
-        {
+//    //    == (Igual)
+//    //    != (Diferente)
+//    //    > (Maior que)
+//    //    < (Menor que)
+//    //    <= (Menor igual)
+//    //    >= (Maior igual)
 
-        }
-    }
-}
+//    //Operador L
+
+//    [SerializeField] int vidaHeroi = 100;
+//    [SerializeField] int vidaVilao = 300;
+//    string resultado;
+
+//    void Start()
+//    {
+//        print(vidaVilao == vidaHeroi); //False
+//        print(vidaHeroi <= vidaVilao); //True
+//        print(vidaVilao != vidaHeroi); //True
+
+//        // Operador ternário
+
+//        //resultado = vidaHeroi < vidaVilao interrogacao "Vida Heroi Menor" : "Vida Heroi Maior";
+
+//        //print(resultado);
+
+//        //Estrutura condicional
+
+//        if (vidaHeroi < vidaVilao)
+//        {
+//            resultado = "Vida Heroi Menor";
+//            print(resultado);
+
+//        }
+//        else if (vidaHeroi == vidaVilao)
+//        {
+//            resultado = "Vida Heroi igual a vida Vilao";
+//            print(resultado);
+//        }
+//        else
+//        { 
+//            resultado = "Vida Heroi Maior";
+//            print(resultado);
+//        }
+//            void Update()
+//            {
+//            }
+
+
+    
