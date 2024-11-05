6ea2f9420c171f8c3c1bcb6ab17b0ba1c8a688dd
vitorhugoutida <vitorhugoutida@gmail.com>
2024-11-05 17:16:56 -0300

    feat: Continuar

--- Assets/Scripts/Atividade07.cs
+++ Assets/Scripts/Atividade07.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class Atividade07 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     // (Dado com N faces) Para fazer um jogo de RPG, vamos precisar calcular probabilidades e simular a rolagem de dados. Crie um script em que o usuário defina o número de faces de um dado (int) e calcule a rolagem de um dado com esse número de faces. Use a função abaixo para o cálculo: Random.Range (valor_min, valor_max); (Coloque o código dentro da função-evento Start).
 
     [SerializeField] int numeroDeFaces;
---
+++ Assets/Scripts/Professor04.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Professor04 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // (Verificar item de inventário) Um jogador possui itens limitados no inventário.Verifique se o jogador possui uma "Poção de Vida". Se possuir, exiba "Usando Poção de Vida
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
