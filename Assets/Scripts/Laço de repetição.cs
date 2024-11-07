ab06047ab805677a677a5c26a80322124f35c5f7
vitorhugoutida <vitorhugoutida@gmail.com>
2024-11-07 17:14:04 -0300

    feat: continuação

---
+++ Assets/Scripts/Laço de repetição.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Laçoderepetição : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //For -> É utilizado quando se sabe a quantidade de repetições
+
+    //While ou DoWhile -> É utilizado quando não se sabe a quantidade de repetições
+
+    //Foreach -> usado em lista ou arrays, para executar uma operação para cada item da lista
+
+    //Array -> É uma lista
+
+
+    void Start()
+    {
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
