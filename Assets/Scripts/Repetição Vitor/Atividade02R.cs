2912878b336a37812ae715670d3ed22255a944b3
vitorhugoutida <vitorhugoutida@gmail.com>
2024-11-09 18:02:51 -0300

    feat: Atividade02R Finalizado

--- Assets/Scripts/Repetição Vitor/Atividade02R.cs
+++ Assets/Scripts/Repetição Vitor/Atividade02R.cs
@@ -1,38 +1,32 @@
-using NUnit.Framework;
-using System.Threading;
 using UnityEngine;
 
 public class Atividade02R : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created,
 
-    //    (Multiplicação de dano) Um personagem ataca cinco vezes, e o
-    //dano do ataque aumenta em 2 a cada vez.Exiba o dano a cada
-    //ataque.
+//    (Multiplicação de dano) Um personagem ataca cinco vezes, e o
+//dano do ataque aumenta em 2 a cada vez.Exiba o dano a cada
+//ataque.
 
-    [SerializeField] int quantiAtaques = 0;
-    [SerializeField] int numeroDeDano = 0;
-    [SerializeField] float segundos;
-
-    async void Start()
+    void Start()
     {
-        while (quantiAtaques >= 5)
+        int dano = 1;
+        int quantiVezes = 0;
+
+        while (quantiVezes < 5)
         {
-            print(numeroDeDano);
-            quantiAtaques++;
-            print(numeroDeDano);
-
-            Thread.Sleep(1000);
-
-            if (quantiAtaques == 5)
-            {
-                numeroDeDano++;
-                quantiAtaques = 0;
-                print("Dano atual: " + numeroDeDano);
-            }
+            print("DANO: " + dano);
+            quantiVezes++;
+            print("QuantiVezes: " + quantiVezes);
+            dano += 2;
+           
         }
+
     }
 
+    void Update()
+    {
+
+    }
 }
-       
+
 
